9d33208a6bc9d4b2330900bd50c39ba4d0a3436e
AdsonEsteves <shinadson@gmail.com>
2021-04-12 03:50:36 -0300

    Adicionar arquivos de projeto.

---
+++ AutoBGchanger/telaSelecaoBG.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace AutoBGchanger
+{
+    partial class telaSelecaoBG
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.boxWidth = new System.Windows.Forms.TextBox();
+            this.boxHeight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxRating = new System.Windows.Forms.ComboBox();
+            this.boxTags = new System.Windows.Forms.TextBox();
+            this.numericUpDownTimer = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.acaoRun);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rating";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tags";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Timer";
+            // 
+            // boxWidth
+            // 
+            this.boxWidth.Location = new System.Drawing.Point(69, 23);
+            this.boxWidth.Name = "boxWidth";
+            this.boxWidth.Size = new System.Drawing.Size(84, 23);
+            this.boxWidth.TabIndex = 5;
+            this.boxWidth.Text = "1920";
+            // 
+            // boxHeight
+            // 
+            this.boxHeight.Location = new System.Drawing.Point(183, 23);
+            this.boxHeight.Name = "boxHeight";
+            this.boxHeight.Size = new System.Drawing.Size(84, 23);
+            this.boxHeight.TabIndex = 6;
+            this.boxHeight.Text = "1080";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "W";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "H";
+            // 
+            // comboBoxRating
+            // 
+            this.comboBoxRating.FormattingEnabled = true;
+            this.comboBoxRating.Items.AddRange(new object[] {
+            "Any",
+            "Safe",
+            "Safe-Questionable",
+            "Questionable",
+            "Questionable-Explicit",
+            "Explicit"});
+            this.comboBoxRating.Location = new System.Drawing.Point(69, 73);
+            this.comboBoxRating.Name = "comboBoxRating";
+            this.comboBoxRating.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxRating.TabIndex = 9;
+            this.comboBoxRating.Text = "Safe";
+            // 
+            // boxTags
+            // 
+            this.boxTags.Location = new System.Drawing.Point(69, 121);
+            this.boxTags.Name = "boxTags";
+            this.boxTags.Size = new System.Drawing.Size(198, 23);
+            this.boxTags.TabIndex = 10;
+            // 
+            // numericUpDownTimer
+            // 
+            this.numericUpDownTimer.Location = new System.Drawing.Point(69, 165);
+            this.numericUpDownTimer.Name = "numericUpDownTimer";
+            this.numericUpDownTimer.Size = new System.Drawing.Size(60, 23);
+            this.numericUpDownTimer.TabIndex = 11;
+            this.numericUpDownTimer.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(135, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Seconds";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // telaSelecaoBG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 251);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDownTimer);
+            this.Controls.Add(this.boxTags);
+            this.Controls.Add(this.comboBoxRating);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.boxHeight);
+            this.Controls.Add(this.boxWidth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "telaSelecaoBG";
+            this.Text = "AutoBackground";
+            this.SizeChanged += new System.EventHandler(this.telaSelecaoBG_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox boxWidth;
+        private System.Windows.Forms.TextBox boxHeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxRating;
+        private System.Windows.Forms.TextBox boxTags;
+        private System.Windows.Forms.NumericUpDown numericUpDownTimer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ AutoBGchanger/telaSelecaoBG.cs
@@ -0,0 +1,167 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoBGchanger
+{
+    public partial class telaSelecaoBG : Form
+    {
+        string apilink = "https://konachan.com/post.json?tags=order%3Arandom";
+        Thread timedBGThread = null;
+        bool running = false;
+        public telaSelecaoBG()
+        {
+            InitializeComponent();
+            notifyIcon1.Icon = SystemIcons.Application;
+        }
+
+        private void acaoRun(object sender, EventArgs e)
+        {
+            if (button1.Text == "Run")
+            {
+                running = true;
+                StringBuilder builder = new StringBuilder();
+                builder.Append(apilink + "+");
+                builder.Append(getTags() + "+");
+                builder.Append(getRating() + "+");
+                builder.Append(getSize());
+                Debug.WriteLine(builder.ToString());
+
+                timedBGThread = new Thread(() => timedLoopBG(builder.ToString()));
+                timedBGThread.Start();
+                button1.Text = "Stop";
+            }
+            else if (button1.Text == "Stop")
+            {
+                running = false;
+                button1.Text = "Run";
+            }
+
+        }
+
+        private void timedLoopBG(string link)
+        {
+            int timer = (int)numericUpDownTimer.Value;
+            try
+            {
+                while (running)
+                {
+                    string linkBG = getBG(link);
+                    changeBG(linkBG);
+                    Thread.Sleep(timer * 1000);
+                }
+            }
+            catch (Exception e)
+            {
+                DialogResult dialogResult = MessageBox.Show("The used tags are not available", "Error", MessageBoxButtons.OK);
+                running = false;
+                button1.Text = "Run";
+            }
+        }
+
+        private void changeBG(string link)
+        {
+            Wallpaper.Set(new Uri(link), Wallpaper.Style.Fill);
+        }
+
+        private string getBG(string formatedLink)
+        {
+            string responseBody = new WebClient().DownloadString(formatedLink);
+
+            dynamic jsonBG = JsonConvert.DeserializeObject(responseBody);
+
+            try
+            {
+                return jsonBG[0].file_url;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        private string getTags()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            string tags = boxTags.Text;
+            builder.Append(tags.Replace(" ", "+"));
+
+            return builder.ToString();
+        }
+
+        private string getSize()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            string width = boxWidth.Text;
+            string height = boxHeight.Text;
+
+            if (!width.Equals(""))
+            {
+                builder.Append("width%3A" + width + "..+");
+            }
+
+            if (!width.Equals(""))
+            {
+                builder.Append("height%3A" + height + "..");
+            }
+
+            return builder.ToString();
+        }
+
+        private string getRating()
+        {
+            string ratingInput = comboBoxRating.SelectedItem == null ? "" : comboBoxRating.SelectedItem.ToString();
+            switch (ratingInput)
+            {
+                case "Safe":
+                    return "rating%3Asafe";
+
+                case "Safe - Questionable":
+                    return "rating%3Aquestionableless";
+
+                case "Questionable":
+                    return "rating%3Aquestionable";
+
+                case "Questionable - Explicit":
+                    return "rating%3Aquestionableplus";
+
+                case "Explicit":
+                    return "rating%3Aexplicit";
+
+                default:
+                    return "";
+            }
+        }
+
+        private void telaSelecaoBG_SizeChanged(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500, "AutoBG", "Running in background", ToolTipIcon.Info);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();            
+        }
+    }
+}
